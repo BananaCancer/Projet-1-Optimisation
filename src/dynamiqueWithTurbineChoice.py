0d6e730f156fe257c3441924c0b7e9cf160ffd24
Courbayre Basil <basil.courbayre@ecole.ensicaen.fr>
2024-03-12 20:03:26 -0400

    Program that takes correct input

--- src/dynamiqueWithTurbineChoice.py
+++ src/dynamiqueWithTurbineChoice.py
@@ -4,19 +4,15 @@ from typing import Dict, List, Union
 import numpy as np
 import pandas as pd
 
-
 MIN_DEBIT = 0
 MAX_DEBIT = 160
 PAS_DEBIT = 5
 
 
 # Paramètres
-Q_TOTAL = 	580 # doit être multiple de PAS_DEBIT sinon erreur
-NIVEAU_AMONT = 	137.89
-TURBINES = [1, 2, 4, 5]
 
 
-NB_TURBINES = len(TURBINES)
+
 # L’élévation avale en fonction du débit total
 # f(x) = p1*x^2 + p2*x + p3	
 
@@ -49,7 +45,7 @@ puissance_turbine_4 = [20.2212 , -0.4586, -0.5777, 0.004886, 0.01151,     0   , 
 puissance_turbine_5 = [1.9786, 0.004009, -0.05699, 0.001064, 0.005456,     0   ,  -8.162e-06,   2.849e-05,     0   ,     0   ]
 		
 
-puissances_turbines = [puissance_turbine_1, puissance_turbine_2, puissance_turbine_3, puissance_turbine_4, puissance_turbine_5]
+ARRAY_COEFFICIENTS_TURBINES = [puissance_turbine_1, puissance_turbine_2, puissance_turbine_3, puissance_turbine_4, puissance_turbine_5]
 
 
 
@@ -58,45 +54,41 @@ puissances_turbines = [puissance_turbine_1, puissance_turbine_2, puissance_turbi
 
 
 
-def calculer_chute_nette(debit_turbine: Union[int,float]) -> Union[int,float]:
+def calculer_chute_nette(debit_turbine: Union[int,float],
+                         q_total: int,
+                         niveau_amont: float) -> Union[int,float]:
   # Vérifier si le débit est dans la plage admissible
   if debit_turbine < MIN_DEBIT or debit_turbine > MAX_DEBIT:
     raise ValueError(f"Le débit doit être compris entre {MIN_DEBIT} et {MAX_DEBIT} m³/s.")
 
   # Déterminer l'élévation avale en fonction du débit total
-  elevation_avale = elevation_avale_en_fonction_du_debit_total[0] * Q_TOTAL**2 + \
-                    elevation_avale_en_fonction_du_debit_total[1] * Q_TOTAL + \
+  elevation_avale = elevation_avale_en_fonction_du_debit_total[0] * q_total**2 + \
+                    elevation_avale_en_fonction_du_debit_total[1] * q_total + \
                     elevation_avale_en_fonction_du_debit_total[2]
 
   # Calculer la chute nette
-  chute_nette = NIVEAU_AMONT - elevation_avale - (0.5 * (10**-5) * (debit_turbine**2))
+  chute_nette = niveau_amont - elevation_avale - (0.5 * (10**-5) * (debit_turbine**2))
 
   return chute_nette
 
 
   
-def getListeEtatsParTurbine(turbines: list) -> Dict[int,List[int]]:
+def getListeEtatsParTurbine(turbines: list,
+                            q_total: int) -> Dict[int,List[int]]:
+  nb_turbines = len(turbines)
   result: Dict[int,List[int]] = {}
-  result[turbines[0]] = [Q_TOTAL]
+  result[turbines[0]] = [q_total]
 
   for i, turbine in enumerate(turbines[1:]):
     # Débit max restant: Max débit * nb turbines restantes 
     # ou QTot si Max débit * nb turbines restantes > Qtot
-    debit_restant_max = MAX_DEBIT*(NB_TURBINES-i-1) if MAX_DEBIT*(NB_TURBINES-i-1) < Q_TOTAL else Q_TOTAL
+    debit_restant_max = MAX_DEBIT*(nb_turbines-i-1) if MAX_DEBIT*(nb_turbines-i-1) < q_total else q_total
     debit_restant_max = round(debit_restant_max,2)
 
     # Débit min restant: Débit total - DébitMax * Nb turbines avant (ou 0)
-    debit_restant_min = Q_TOTAL - MAX_DEBIT*(i+1) if (Q_TOTAL - MAX_DEBIT*(i+1)) > 0 else 0
+    debit_restant_min = q_total - MAX_DEBIT*(i+1) if (q_total - MAX_DEBIT*(i+1)) > 0 else 0
     debit_restant_min = round(debit_restant_min,2)
 
-    #Before
-    """
-    debit_restant = list(np.arange(debit_restant_min,
-                                   debit_restant_max + PAS_DEBIT,
-                                   PAS_DEBIT))
-    debit_restant = [value for value in debit_restant if value < debit_restant_max]
-    """
-    #Now
     debit_restant = list(np.arange(debit_restant_min,
                                    debit_restant_max,
                                    PAS_DEBIT))
@@ -105,11 +97,11 @@ def getListeEtatsParTurbine(turbines: list) -> Dict[int,List[int]]:
     result[turbine]= debit_restant
   return result
 
-def list_debit_turbine():
+def list_debit_turbine(q_total: int):
    """
    return Debit de la turbine possible
    """
-   max_for_turbine = Q_TOTAL if MAX_DEBIT > Q_TOTAL else MAX_DEBIT
+   max_for_turbine = q_total if MAX_DEBIT > q_total else MAX_DEBIT
    debit_turbine = list(np.arange(MIN_DEBIT,max_for_turbine+PAS_DEBIT,PAS_DEBIT))
    debit_turbine[-1] = max_for_turbine
    return debit_turbine
@@ -172,7 +164,9 @@ def powerFunction(debit_turbine: Union[int,float],
 
 def fillLastStage(stage: pd.DataFrame,
                   fonctionPuissance: list,
-                  turbineID: int = 5) -> pd.DataFrame: 
+                  turbineID: int,
+                  q_total,
+                  niveau_amont) -> pd.DataFrame: 
   """
   debit_restant : lignes
   debit_turbine : columns
@@ -190,7 +184,7 @@ def fillLastStage(stage: pd.DataFrame,
     debit_turbine = debit_restant
     stage.loc[debit_restant, xn] = debit_restant
     
-    chute_nette = calculer_chute_nette(debit_turbine=debit_turbine)
+    chute_nette = calculer_chute_nette(debit_turbine, q_total, niveau_amont)
     puissance = powerFunction(debit_turbine, chute_nette, fonctionPuissance)
     stage.loc[debit_restant, fn] = puissance
   return stage
@@ -202,7 +196,10 @@ def fillPreviousStages(stage: pd.DataFrame,
                        fonctionPuissance: list,
                        turbineID: int,
                        previousTurbineID: int,
-                       turbineIndex: int) -> pd.DataFrame: 
+                       turbineIndex: int,
+                       q_total: int,
+                       niveau_amont: float,
+                       nb_turbines: int) -> pd.DataFrame: 
   """
   debit_restant : lignes
   debit_turbine : columns
@@ -216,12 +213,12 @@ def fillPreviousStages(stage: pd.DataFrame,
   """
   
 
-  debit_restant_max_for_turbine_after = MAX_DEBIT * (NB_TURBINES - turbineIndex) if MAX_DEBIT*(NB_TURBINES - turbineIndex) < Q_TOTAL else Q_TOTAL
+  debit_restant_max_for_turbine_after = MAX_DEBIT * (nb_turbines - turbineIndex) if MAX_DEBIT*(nb_turbines - turbineIndex) < q_total else q_total
   debit_turbine_columns = stage.columns
   addOptimalResultCols(stage, turbineID)
   for debit_restant in stage.index:
     for debit_turbine in debit_turbine_columns:
-      chute_nette = calculer_chute_nette(debit_turbine=debit_turbine)
+      chute_nette = calculer_chute_nette(debit_turbine, q_total, niveau_amont)
 
       debit_for_turbine_after = debit_restant-debit_turbine
       if debit_for_turbine_after < 0 or debit_for_turbine_after > debit_restant_max_for_turbine_after:
@@ -255,41 +252,93 @@ def forward_pass(stage : pd.DataFrame,
   final_df = df_tableau_turbine_after.loc[[debit_restant_after]]
   return final_df
 
-
-if __name__ == "__main__":
-  # ====
-  listeEtats = getListeEtatsParTurbine(turbines = TURBINES)
-  # # ====
-  debits_possible = list_debit_turbine()
-  emptyStages = createStagesDF(turbines = TURBINES, listeEtats = listeEtats,
-                              debits_possible = debits_possible)
-
+def backwardPass(turbines: list, q_total: int, niveau_amont: float, emptyStages):
+  nb_turbines = len(turbines)
   filledStages = {}
-  for i, turbineID in enumerate(TURBINES[::-1]):
-    if turbineID == TURBINES[-1]:
-      filledStages[turbineID] = fillLastStage(stage = copy.copy(emptyStages[turbineID]),
-                                      fonctionPuissance = puissances_turbines[turbineID - 1],
-                                      turbineID=turbineID)
+  for i, turbineID in enumerate(turbines[::-1]):
+    if turbineID == turbines[-1]:
+      filledStages[turbineID] = fillLastStage(copy.copy(emptyStages[turbineID]),
+                                      ARRAY_COEFFICIENTS_TURBINES[turbineID - 1],
+                                      turbineID,
+                                      q_total,
+                                      niveau_amont)
     else:
-      filledStages[turbineID] = fillPreviousStages(stage=copy.copy(emptyStages[turbineID]),
-                                           previousStage=copy.copy(filledStages[prevStage]),
-                                           fonctionPuissance=puissances_turbines[turbineID - 1],
-                                           turbineID=turbineID,
-                                           previousTurbineID = prevStage,
-                                           turbineIndex = NB_TURBINES - i)
+      filledStages[turbineID] = fillPreviousStages(copy.copy(emptyStages[turbineID]),
+                                           copy.copy(filledStages[prevStage]),
+                                           ARRAY_COEFFICIENTS_TURBINES[turbineID - 1],
+                                           turbineID,
+                                           prevStage,
+                                           nb_turbines - i,
+                                           q_total,
+                                           niveau_amont,
+                                           nb_turbines)
     prevStage = turbineID
-  dfs_choose = {}
-  prevStage = TURBINES[0]
-  dfs_choose[prevStage] = copy.copy(filledStages[prevStage])
+  return filledStages
 
-  for i in TURBINES[1:]:
-    dfs_choose[i] = forward_pass(stage = copy.copy(dfs_choose[prevStage]),
+def loopForwardPass(turbines, filledStages):
+    dfs_choose = {}
+    prevStage = turbines[0]
+    dfs_choose[prevStage] = copy.copy(filledStages[prevStage])
+
+    for i in turbines[1:]:
+      dfs_choose[i] = forward_pass(stage = copy.copy(dfs_choose[prevStage]),
                                  df_tableau_turbine_after = copy.copy(filledStages[i]),
                                  number_to_choose = i,
                                  prevStageID = prevStage)
-    prevStage = i
+      prevStage = i
+    return dfs_choose
+
+def algo(q_total, niveau_amont, turbines):
+  listeEtats = getListeEtatsParTurbine(turbines, q_total)
+  debits_possible = list_debit_turbine(q_total)
+  emptyStages = createStagesDF(turbines, listeEtats, debits_possible)
+  filledStages = backwardPass(turbines, q_total, niveau_amont, emptyStages)
+  dfs_choose = loopForwardPass(turbines, filledStages)
   
-  print(" ========================================= ")
-  for i in TURBINES:
-    print(f"turbine {i} : Q{i} = {dfs_choose[i]['X'+str(i)].values[0]}")
-    print(dfs_choose[i])
+  return dfs_choose
+
+
+
+if __name__ == "__main__":
+  q_total = 	580 # doit être multiple de PAS_DEBIT sinon erreur
+  niveau_amont = 	137.89
+  turbines = [1, 2, 4, 5]
+  #algo(q_total, niveau_amont, turbines)
+  df = pd. read_excel("data/DataProjet2024.xlsx")
+  STARTING_ROW = 2
+  ROW_COUNT = 100
+  for row in range(STARTING_ROW, STARTING_ROW + ROW_COUNT + 1):
+    debit_total = round(df.iloc[row, 2] / 5) * 5
+    niveau_amont = df.iloc[row, 5]
+    actives_turbines = []
+    debits_turbines = {}
+    puissances_turbines = {}
+    sumPuissance = 0
+    sumComputedPuissance = 0
+    for i in range(5):
+      if int(df.iloc[row, 6 + 2 * i]) != 0:
+        actives_turbines.append(i+1)
+        debits_turbines[i + 1] = df.iloc[row, 6 + 2 * i]
+        puissances_turbines[i + 1] = df.iloc[row, 7 + 2 * i]
+        sumPuissance += df.iloc[row, 7 + 2 * i]
+
+    result = algo(debit_total,niveau_amont, actives_turbines)
+    sumDifferences = 0
+    for i, IDturbine in enumerate(actives_turbines):
+      if (i < len(actives_turbines) - 1):
+        nextTurbine = actives_turbines[i+1]
+        currentPuissance = result[IDturbine]['F' + str(IDturbine)].values[0] - result[nextTurbine]['F'+str(nextTurbine)].values[0]
+      else:
+        currentPuissance = result[IDturbine]['F' + str(IDturbine)].values[0]
+      sumComputedPuissance += currentPuissance
+      currentDebit = result[IDturbine]['X'+str(IDturbine)].values[0]
+      print(f"Turbine {IDturbine}")
+      print(f"Débit utilisé : {debits_turbines[IDturbine]} (réelle) vs {currentDebit} (calculée)")
+      print(f"Puissance produite : {puissances_turbines[IDturbine]} (réelle) vs {currentPuissance} (calculée)")
+      print(" ========================================= ")
+    print(f"Puissance totale produite : {sumPuissance} (réelle) vs {sumComputedPuissance}")
+    sumDifferences += sumComputedPuissance - sumPuissance
+  print(sumDifferences)
+    
+    
+    
